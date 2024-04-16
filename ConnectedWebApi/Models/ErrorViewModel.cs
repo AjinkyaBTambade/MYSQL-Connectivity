449502d6eb6f3d7b6eabb30f15fde23a67497fff
Ajinkya <ajinkyatambade143@gmail.com>
2024-04-16 10:44:32 +0530

    Connected Architecture Added

---
+++ ConnectedWebApi/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ConnectedWebApi.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
--- DisconnectedWebApi/Program.cs
+++ DisconnectedWebApi/Program.cs
@@ -26,3 +26,8 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
+
+
+
