3fc378f0157f80b0f3f25bb93dca3f4ccd5b86ea
Ajinkya <ajinkyatambade143@gmail.com>
2024-05-19 00:32:43 +0530

    Dapper

---
+++ Dapper/Entities/Project.cs
@@ -0,0 +1,12 @@
+namespace TFLPortal.Entities;
+
+
+public class Project
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public string? Status { get; set; }
+}
--- DisconnectedWebApi/Repositories/IProjectRepository.cs
+++ DisconnectedWebApi/Repositories/IProjectRepository.cs
@@ -3,10 +3,8 @@ using TFLPortal.Entities;
 namespace TFLPortal.Repositories.Interfaces;
 public interface IProjectRepository{
 
-  
 
     public  Task<List<Project>> GetProjects();
-
     public Project GetProject(int id);
     public bool Insert(Project project);
     public  bool Update(Project project);
