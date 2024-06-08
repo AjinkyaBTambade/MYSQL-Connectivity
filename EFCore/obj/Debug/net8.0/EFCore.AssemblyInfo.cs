7f334960f2e1cf82fc492d180b0abd28da69a865
Ajinkya <ajinkyatambade143@gmail.com>
2024-06-08 17:28:58 +0530

    Changes

--- EFCore/obj/Debug/net8.0/EFCore.AssemblyInfo.cs
+++ EFCore/obj/Debug/net8.0/EFCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EFCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d3cd54f5b0d065e2d9684c7c713084e434e859b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9694e3b78b72631dccc8fb692a6c82ad2b513fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("EFCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EFCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
